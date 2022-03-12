151bd864c70e69fa0bc6707d190faa1e070b59fe
Balaji <59250390+scamanderoffl@users.noreply.github.com>
2022-03-12 22:04:15 +0530

    Add files via upload

---
+++ Program/Basic/LED Blink.X/main.c
@@ -0,0 +1,42 @@
+/*
+ * File:   main.c
+ * Author: scamanderoffl
+ *
+ * Created on 12 March, 2022, 3:30 PM
+ */
+
+
+#include <xc.h>
+#pragma config WDTE = OFF  //Watchdog Timer Enable bit (WDT disabled)
+
+void init_config(void)
+{
+    //set PORTD as output
+    TRISD = 0x00;
+    
+    //set all LEDs OFF
+    PORTD = 0x00;
+}
+
+void main(void)
+{
+    unsigned long int delay;
+    init_config();
+    
+    while(1)
+    {
+        //Turn ON LED
+        PORTD = 0x01; //0000 0001
+        
+        //Delay
+        for(delay=0;delay<50000;delay++);
+        
+        //Turn OFF LED
+        PORTD= 0x00;//1111 0000
+        
+        //Delay
+        for(delay=0;delay<50000;delay++);
+        
+    }
+    return;
+}
